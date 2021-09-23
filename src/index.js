2fcc58ebd78e0856f23b6d0460064f909d967d42
Ali Burzhi <aliburzhi033@gmail.com>
2021-09-23 15:27:59 +0300

    fetch pokemon

--- src/index.js
+++ src/index.js
@@ -1,1 +1,12 @@
 import './css/common.css';
+
+fetch('https://pokeapi.co/api/v2/pokemon/1')
+    .then(response => {
+        return response.json();
+    })
+    .then(pokemon => {
+        console.log(pokemon);
+    })
+    .catch(error => {
+        console.log(error);
+    });
